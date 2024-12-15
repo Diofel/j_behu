60b25395bb059ec3d8d33b2845ab16563b371dd7
myntchoco <belenyuan1@gmail.com>
2024-12-15 23:06:49 +0800

    Updated sound effects and integrated into game logic.

--- WavPlayer.java
+++ WavPlayer.java
@@ -20,9 +20,12 @@ public class WavPlayer {
         startInteractiveMenu();
     }
 
+//lobby music kakegurui op1 instrumental
     public static void lobbyMusic() {
-        playAudio("game_assets/intro1.wav"); // Play the intro1.wav file from the game_assets folder
+        playAudio("game_assets/sfx/intro1.wav"); // Play the intro1.wav file from the game_assets folder
     }
+
+    //add more music if needed
 
     public static void startInteractiveMenu() {
         Scanner scanner = new Scanner(System.in);
@@ -71,5 +74,77 @@ public class WavPlayer {
             }
         }
         clips.clear();
+    }
+
+    //UI Sounds
+    public static void playClick() {
+        playAudio("game_assets/sfx/click.wav");
+    }
+
+    public static void playCharacterSelect() {
+        playAudio("game_assets/sfx/character_select.wav");
+    }
+
+    //Rock Paper Scissors Sounds
+    public static void playRock() {
+        playAudio("game_assets/sfx/rock.wav");
+    }
+
+    public static void playPaper() {
+        playAudio("game_assets/sfx/paper.wav");
+    }
+
+    public static void playScissors() {
+        playAudio("game_assets/sfx/scissors.wav");
+    }
+
+    public static void playVersus() {
+        playAudio("game_assets/sfx/versus.wav");
+    }
+
+    public static void playWinRound() {
+        playAudio("game_assets/sfx/win_round.wav");
+    }
+
+    public static void playLoseRound() {
+        playAudio("game_assets/sfx/lose_round.wav");
+    }
+
+    public static void playTie() {
+        playAudio("game_assets/sfx/tie.wav");
+    }
+
+    //Game Outcome Sounds
+    public static void playWinGame() {
+        playAudio("game_assets/sfx/win_game.wav");
+    }
+
+    public static void playLoseGame() {
+        playAudio("game_assets/sfx/lose_game.wav");
+    }
+
+    //Fighting Game Sounds
+    public static void playPlayerAttack() {
+        playAudio("game_assets/sfx/player_attack.wav");
+    }
+
+    public static void playPlayerDamaged() {
+        playAudio("game_assets/sfx/player_damaged.wav");
+    }
+
+    public static void playEnemyAttack() {
+        playAudio("game_assets/sfx/enemy_attack.wav");
+    }
+
+    public static void playEnemyDamaged() {
+        playAudio("game_assets/sfx/enemy_damaged.wav");
+    }
+
+    public static void playHeartbeat() {
+        playAudio("game_assets/sfx/heartbeat.wav");
+    }
+
+    public static void playLifeLost() {
+        playAudio("game_assets/sfx/life_lost.wav");
     }
 }
